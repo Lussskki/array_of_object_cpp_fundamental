1acbd973cdd9a2aaf7ff298b94ec69daac5ca6ef
Luka <lukaguledani123@gmail.com>
2025-10-15 16:02:20 +0400

    create class of student and save objects in class

--- array_object_cpp_fundamental/array_object.cpp
+++ array_object_cpp_fundamental/array_object.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
 using namespace std;
 
+class Student
+{
+public:
+	string name;
+	int age;
+	char gender;
+	float programming_grade;
+
+	Student()
+	{ }
+
+	Student(string name, int age, char gender, float programming_grade)
+	{
+		this->name = name;
+		this->age = age;
+		this->gender = gender;
+		this->programming_grade = programming_grade;
+	}
+};
+
 int main()
 {
-	cout << "Hello World" << endl;
+	/*int lucky_numbers[5] = { 1,2,3,4,5 };*/
+	Student  students[3];
+
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "STUDENT "<< i + 1 << endl;
+		Student s;
+
+		cout << "Name: ";
+		cin >> s.name;
+		cout << "Age: ";
+		cin >> s.age;
+		cout << "Gender: ";
+		cin >> s.gender;
+		cout << "Programming Grade: ";
+		cin >> s.programming_grade;
+
+		students[i] = s;
+	}
+
+	cin.get();
 }
